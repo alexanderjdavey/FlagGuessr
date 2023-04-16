f0cbad18136943624c1eb1ef52d50a8e3e856fb3
Alex <76870578+alexanderjdavey@users.noreply.github.com>
2023-04-16 01:29:10 +0100

    Full Commit

---
+++ script.js
@@ -0,0 +1,113 @@
+let flags = [];
+let remainingFlags;
+let currentFlag;
+let correctGuesses = 0;
+let incorrectGuesses = 0;
+let timerInterval;
+let timer = 0;
+
+document.getElementById("start-button").addEventListener("click", startGame);
+document.getElementById("reset-button").addEventListener("click", resetGame);
+document.getElementById("country-input").addEventListener("change", checkGuess);
+document.getElementById("skip-button").addEventListener("click", skipFlag);
+
+// Fetch flag data from flags.json
+fetch("flags.json")
+  .then((response) => response.json())
+  .then((data) => {
+    flags = data;
+  });
+
+function startGame() {
+  remainingFlags = [...flags];
+  correctGuesses = 0;
+  incorrectGuesses = 0;
+  timer = 0;
+  updateCorrectGuesses();
+  updateIncorrectGuesses();
+  pickNewFlag();
+  document.getElementById("start-button").disabled = true;
+  document.getElementById("flag-container").style.display = "flex";
+  startTimer();
+  document.getElementById("playbuttons").style.display = "flex";
+  document.getElementById("infotext").style.display = "block";
+}
+
+function resetGame() {
+  document.getElementById("playbuttons").style.display = "none";
+  document.getElementById("infotext").style.display = "none";
+  remainingFlags = [];
+  currentFlag = undefined;
+  correctGuesses = 0;
+  incorrectGuesses = 0;
+  timer = 0;
+  updateCorrectGuesses();
+  updateIncorrectGuesses();
+  document.querySelector(".flag-img").src = "";
+  document.getElementById("country-input").value = "";
+  document.getElementById("start-button").disabled = false;
+  document.getElementById("flag-container").style.display = "none";
+  stopTimer();
+  input.style.backgroundColor = "#ffffff";
+}
+
+function startTimer() {
+  timerInterval = setInterval(function () {
+    timer++;
+    document.getElementById("timer").innerText = timer;
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+}
+
+function updateIncorrectGuesses() {
+  document.getElementById("incorrect-guesses").innerText = incorrectGuesses;
+}
+
+function skipFlag() {
+  if (remainingFlags.length > 0) {
+    incorrectGuesses++;
+    updateIncorrectGuesses();
+    pickNewFlag();
+  }
+}
+
+function pickNewFlag() {
+  const randomIndex = Math.floor(Math.random() * remainingFlags.length);
+  currentFlag = remainingFlags[randomIndex];
+  remainingFlags.splice(randomIndex, 1);
+  document.querySelector(".flag-img").src = currentFlag.imageUrl;
+}
+
+function checkGuess() {
+  const input = document.getElementById("country-input");
+  if (
+    input.value.toLowerCase() === currentFlag.country.toLowerCase() ||
+    currentFlag.alternatives.includes(input.value)
+  ) {
+    correctGuesses++;
+    updateCorrectGuesses();
+    input.value = "";
+    input.style.backgroundColor = "white";
+    if (remainingFlags.length > 0) {
+      pickNewFlag();
+    } else {
+      document.querySelector(".flag-img").src = "";
+      alert(
+        `You've finished in ${timer} seconds with ${incorrectGuesses} skips and ${correctGuesses} correct guesses!`
+      );
+      document.getElementById("playbuttons").style.display = "none";
+      document.getElementById("infotext").style.display = "none";
+      document.getElementById("start-button").disabled = false;
+      document.getElementById("flag-container").style.display = "none";
+    }
+  } else {
+    input.style.backgroundColor = "#ffabab";
+  }
+}
+
+function updateCorrectGuesses() {
+  document.getElementById("correct-guesses").innerText = correctGuesses;
+}
